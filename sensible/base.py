c8202f55811b63cbfb81b8353ae96f24e28d38b9
ernestkz <ernestkuznecovs@gmail.com>
2021-11-18 14:00:28 +0000

    Comments on project.

--- sensible/base.py
+++ sensible/base.py
@@ -3,16 +3,23 @@ sensible base module.
 
 This is the principal module of the sensible project.
 here you put your main classes and objects.
-
-Be creative! do whatever you want!
-
-If you want to replace this with a Flask application run:
-
-    $ make init
-
-and then choose `flask` as template.
 """
 
+
+# Device:
+# - One flexible core protocol.
+#   - Can extablish details about communication to other devices.
+
+# - The device simply accumulates information of the world by collecting  sensor data from other devices.
+# - Other devices can share the sensor data that they have collected from other devices aswell.
+
+# Core functionality:
+# - Discover other devices.
+# - Establish communication protocol with them.
+#   - The resulting communication protocol is set by the user depending on requirements.
+#   - The resulting communciation protocol is also dependent on the other device.
+# - Recieve, send, and store sensor data.
+# - The stored sensor data can be queried and inspected.
 
 class BaseClass:
     def base_method(self) -> str:
@@ -23,7 +30,6 @@ class BaseClass:
 
     def __call__(self) -> str:
         return self.base_method()
-
 
 def base_function() -> str:
     """
