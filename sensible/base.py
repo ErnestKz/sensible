3d1005b354a079da344c8141a7de46b98ffe27e0
ernestkz <ernestkuznecovs@gmail.com>
2021-11-19 01:24:54 +0000

    Multi connection.

--- sensible/base.py
+++ sensible/base.py
@@ -97,6 +97,7 @@ def base_echo_client() -> None:
     print("Received", repr(data))
     return None
 
+# Multi-Connection Server:
 def base_echo_server_multi() -> None:
     """
     base_echo_server_multi :: IO ()
@@ -105,7 +106,7 @@ def base_echo_server_multi() -> None:
     PORT = 65432
 
     sel = selectors.DefaultSelector()
-    
+    # ....
     lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     lsock.bind((HOST, PORT))
     lsock.listen()
@@ -119,5 +120,88 @@ def base_echo_server_multi() -> None:
     # data is used to store arbitrary data along with the socket
     
     return None
-
     
+def base_event_loop() -> None:
+    sel = selectors.DefaultSelector()
+    # ...
+    while True:
+        events = sel.select(timeout=None) # Blocks until there are sockets ready for IO
+        for key, mask in events:           
+            if key.data is None:            # data is the data that what we attatched to the event
+                accept_wrapper(key.fileobj) # fileobj is the socket object
+                # call our accept(), to get new socket object and register with the selector
+            else:
+                service_connection(key, mask) # This services the accepted connection
+    
+def accept_wrapper(sock) -> None:
+    conn, addr = sock.accept()  # should be ready to read
+    print("Accepted connection from", addr)
+    conn.setblocking(False)
+
+    # we create an object to hold data we want included along with the socket
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask) -> None:
+    sock = key.fileobj
+    data = key.data
+    # looks like the mask that we pass will always read and then write
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print("Closing connection to", data_addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print("Echoing", repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb) # Should be ready to write
+            data.outb = data.outb[sent:]
+    
+# MultiConnection Client:
+messages = [b'Message 1 from client.', b'Message 2 from client.']
+
+def client_start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print("Starting connection", connid, "to", server_addr)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(connid=connid,
+                                     msg_total=sum(len(m) for m in messages),
+                                     recv_total=0,
+                                     messages=list(messages),
+                                     outb=b'')
+        sel.register(sock, events, data=data)
+        
+            
+def client_service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) # should be ready to read
+        if recv_data:
+            print('received', repr(recv_data), 'from connection', data.connid)
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print('closing connection', data.connid)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print('sending', repr(data.outb), 'to connection', data.connid)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+    
+# Client keeps track the number of bytes it's received from the server so it
+# can close its side of the connection.
+# When the server detects this, it closes its side of the connection too.
+# This depends on the client being well behaved
